897a0200b89517c7c384c923c5650ea9751c2859
s0u8a <mitoharu42024202@gmail.com>
2026-03-25 14:28:51 +0900

    修正

--- script/config.js
+++ script/config.js
@@ -2,5 +2,5 @@
 const CONFIG = {
     CLOUDINARY_CLOUD_NAME: 'djhjyfe3k',
     CLOUDINARY_PRESET: 'my_preset',
-    GOOGLE_VISION_API_KEY: 'AIza...' // ★ここに本物のキーを書く
+    GOOGLE_VISION_API_KEY: 'AIzaSyDAfzLdri00Mghw-5jO6-ubYp66ZHxVJ1A' // ★ここに本物のキーを書く
 };
